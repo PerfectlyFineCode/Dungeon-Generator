6242b03a68915afc7fc20adb9202cdac9881926d
ADEL\spp109 <martin.magnusson@adelfors.nu>
2021-10-20 13:54:39 +0200

    Initial commit

---
+++ src/Dungeon Generation/.idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,3 @@
+
+Q
+!Assets/Scripts/Grid/GridSystem.cs,b\9\b9935863057f1d877aa0627d7b6a7ba3db7a78e8
---
+++ src/Dungeon Generation/Assets/Scripts/Grid/GridSystem.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridSystem
+{
+	public static Node[,] Nodes { get; private set; }
+
+	public static void Initialize(int x, int y)
+	{
+		Nodes = new Node[x, y];
+	}
+
+	public static void Resize(int x, int y)
+	{
+	}
+
+	public static void SetSize(int a, int b)
+	{
+	}
+}
+
+public class Node
+{
+	public bool Blocking { get; private set; }
+
+	public void SetBlocking(bool blocking)
+	{
+		Blocking = blocking;
+	}
+}
