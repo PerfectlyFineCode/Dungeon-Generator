b26c25348badd5012bc55bfc6263864bb3e7d081
VectorArtGames <70201838+VectorArtGames@users.noreply.github.com>
2021-10-26 12:19:14 +0200

    Remade generation
    - Now I just need to get the difference between bounds A and B.

--- src/Dungeon Generation/Assets/Scripts/Generator/DungeonGenerator.cs
+++ src/Dungeon Generation/Assets/Scripts/Generator/DungeonGenerator.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,56 +7,74 @@ public class DungeonGenerator : MonoBehaviour
 {
 	private const int MaxRetries = 200;
 	private const int MaxDepth = 1000;
+	private readonly Dictionary<int, Bounds> BoundsList = new Dictionary<int, Bounds>();
+
+	private List<RoomInformation> CurrentRooms;
+
+	private void OnDrawGizmos()
+	{
+		if (BoundsList != null)
+			foreach (Bounds bounds in BoundsList.Values)
+				Gizmos.DrawWireCube(bounds.center, bounds.size);
+
+		if (CurrentRooms == null) return;
+		foreach (RoomInformation info in CurrentRooms)
+		{
+			if (info.Parent == null) continue;
+			Gizmos.DrawLine(info.transform.position, info.Parent.transform.position);
+		}
+	}
 
 	public void Generate(Vector3 startPoint, RoomCollectionData data)
 	{
 		if (data.StartRoom.Room == null) return;
-		var startRoom = Instantiate(data.StartRoom.Room);
+		GameObject startRoom = Instantiate(data.StartRoom.Room);
 		if (!startRoom.TryGetComponent(out RoomInformation information)) return;
 		information.Name = "Start";
-		CurrentRooms     = new List<RoomInformation>(new[] { information });
+		CurrentRooms = new List<RoomInformation>(new[] { information });
 		StartCoroutine(GenerateRoom(data, information, information));
 	}
-
-	private List<RoomInformation> CurrentRooms;
-	private Dictionary<int, Bounds> BoundsList = new Dictionary<int, Bounds>();
 
 	private IEnumerator GenerateRoom(RoomCollectionData data, RoomInformation startRoom,
 		RoomInformation previousRoom = null, int currentDepth = 0)
 	{
-		if (currentDepth >= MaxDepth) yield break;
-		Bounds   currentBounds = default;
-		RoomData currentRoom   = null;
-		var      retries       = 0;
+		if (currentDepth > MaxDepth) yield break;
+		Bounds currentBounds = default;
+		RoomData currentRoom = null;
+		var retries = 0;
 		BoundsList.Add(currentDepth, currentBounds);
 		do
 		{
-			if (retries >= MaxRetries) break;
+			if (retries > MaxRetries) break;
 			retries++;
-			var roomData = data.Rooms.Random();
-			var endPoint = roomData.GetRoomBounds.RandomPoint();
+			RoomData roomData = data.Rooms.Random();
+			Vector3 endPoint = roomData.GetRoomBounds.center;
 
-			var startPoint = startRoom.GetRandomPath;
+			RoomInformation start = previousRoom == null ? startRoom : previousRoom;
+			Vector3 startPoint = start.GetRandomPath;
 
-			var roomSize           = roomData.GetRoomBounds.size;
-			var currentRoomExtents = startRoom.Bounds.extents;
-			var roomExtents        = roomData.GetRoomBounds.extents;
 
-			var pos = startPoint + endPoint;
+			Vector3 roomSize = roomData.GetRoomBounds.size;
+			Vector3 currentRoomExtents = startRoom.Bounds.extents;
+			Vector3 roomExtents = roomData.GetRoomBounds.extents;
+
+			Vector3 pos = startPoint + endPoint;
+
 
 			currentBounds = new Bounds(pos, roomSize);
 			Debug.Log("Generating new ..");
 			BoundsList[currentDepth] = currentBounds;
-			yield return new WaitForSeconds(0.5f);
+			yield return new WaitForSeconds(0.05f);
 			currentRoom = roomData;
 		} while (IntersectsAny(CurrentRooms, currentBounds));
 
 		if (currentRoom == null || IntersectsAny(CurrentRooms, currentBounds)) yield break;
 
-		var roomObject      = Instantiate(currentRoom.Room, currentBounds.center, Quaternion.identity);
+		GameObject roomObject = Instantiate(currentRoom.Room, currentBounds.center - currentRoom.GetRoomBounds.center,
+			Quaternion.identity);
 		var roomInformation = roomObject.GetComponent<RoomInformation>();
 		roomInformation.Parent = previousRoom;
-		roomInformation.Name   = $"{currentDepth}";
+		roomInformation.Name = $"{currentDepth}";
 		CurrentRooms.Add(roomInformation);
 		StartCoroutine(GenerateRoom(data, startRoom, roomInformation, currentDepth + 1));
 		Debug.Log("Success");
@@ -66,19 +83,5 @@ public class DungeonGenerator : MonoBehaviour
 	private static bool IntersectsAny(IEnumerable<RoomInformation> informations, Bounds bounds)
 	{
 		return informations.Any(room => room.Bounds.Intersects(bounds));
-	}
-
-	private void OnDrawGizmos()
-	{
-		if (BoundsList != null)
-			foreach (var bounds in BoundsList.Values)
-				Gizmos.DrawWireCube(bounds.center, bounds.size);
-
-		if (CurrentRooms == null) return;
-		foreach (var info in CurrentRooms)
-		{
-			if (info.Parent == null) continue;
-			Gizmos.DrawLine(info.transform.position, info.Parent.transform.position);
-		}
 	}
 }
--- src/Dungeon Generation/Assets/Scripts/Room/Information/RoomInformation.cs
+++ src/Dungeon Generation/Assets/Scripts/Room/Information/RoomInformation.cs
@@ -1,44 +1,44 @@
 using System;
 using System.Collections.Generic;
-using System.Drawing;
-using System.IO;
 using System.Linq;
 using UnityEditor;
 using UnityEditor.IMGUI.Controls;
 using UnityEngine;
-using Color = UnityEngine.Color;
 
 public class RoomInformation : MonoBehaviour
 {
-	[field: SerializeField] public RoomInformation Parent { get; set; }
+	[field: SerializeField]
+	public RoomInformation Parent { get; set; }
 
 	public Bounds Bounds
 	{
 		get
 		{
 			var bounds = new Bounds(transform.position, Vector3.zero);
-			foreach (var child in GetComponentsInChildren<MeshRenderer>()) bounds.Encapsulate(child.bounds);
+			foreach (MeshRenderer child in GetComponentsInChildren<MeshRenderer>()) bounds.Encapsulate(child.bounds);
 			return bounds;
 		}
 	}
 
-	[field: SerializeField] public string Name { get; set; }
+	[field: SerializeField]
+	public string Name { get; set; }
 
-	[field: SerializeField] public List<PathBounds> PathBounds { get; set; }
+	[field: SerializeField]
+	public List<PathBounds> PathBounds { get; set; }
 
 	public Vector3 GetRandomPath
 	{
 		get
 		{
-			var path = PathBounds.Select(x => x.Bounds.RandomPoint()).Random();
-			return transform.position + new Vector3(path.x, 0, path.z);
+			Vector3 path = PathBounds.Select(x => x.Bounds.center).Random();
+			return new Vector3(path.x, 0, path.z);
 		}
 	}
 
 	private void OnDrawGizmos()
 	{
 		var bounds = new Bounds(transform.position, Vector3.zero);
-		foreach (var child in GetComponentsInChildren<MeshRenderer>()) bounds.Encapsulate(child.bounds);
+		foreach (MeshRenderer child in GetComponentsInChildren<MeshRenderer>()) bounds.Encapsulate(child.bounds);
 
 		Gizmos.color = Color.red;
 		Gizmos.DrawWireCube(bounds.center, bounds.size);
@@ -59,5 +59,6 @@ public class PathBounds
 {
 	[HideInInspector] public BoxBoundsHandle BoundsHandle = new BoxBoundsHandle();
 
-	[field: SerializeField] public Bounds Bounds { get; set; }
+	[field: SerializeField]
+	public Bounds Bounds { get; set; }
 }
